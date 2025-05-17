99b3b11a4a97158d2c5ba3c57ac07bdc4f37ec29
guest01010 <MOHAMEDAMINE.OUAZZOU.ESTN24@ump.ac.ma>
2025-05-17 18:00:41 +0100

    Update DataStorage.java

--- DataStorage.java
+++ DataStorage.java
@@ -8,29 +8,28 @@ public class DataStorage {
     private static Map<String, Owner> owners = new HashMap<>();
     public static List<Veterinarian> veterinarians = new ArrayList<>();
 
-    // Ajouter un nouveau propriétaire à la map
+
     public static void addOwner(Owner owner) {
         owners.put(owner.getFullName().toLowerCase(), owner);
     }
 
-    // Supprimer un propriétaire de la map
     public static void removeOwner(String fullName) {
         owners.remove(fullName.toLowerCase());
     }
 
-    // Rechercher des propriétaires par nom de famille
+
     public static List<Owner> searchOwnersByLastName(String lastName) {
         return owners.values().stream()
                 .filter(o -> o.getFullName().toLowerCase().contains(lastName.toLowerCase()))
                 .collect(Collectors.toList());
     }
 
-    // Obtenir tous les propriétaires sous forme de liste
+ 
     public static List<Owner> getOwners() {
         return new ArrayList<>(owners.values());
     }
 
-    // Initialiser une liste de vétérinaires fictifs
+
     public static void initVeterinarians() {
         veterinarians.add(new Veterinarian("Marıam", "Smith", "Dentisterie"));
         veterinarians.add(new Veterinarian("John", "Miller", "Chirurgie"));
